0380f1d40e959c9b9289cc708fdd07568038db73
DJ Neill <dj.neill856@gmail.com>
2024-09-14 17:47:07 -0500

    start creating custom colors and fonts for tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,32 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'beige-500': "#98908B",
+        'beige-100': "#F8Ff4F0",
+        'grey-900': '#201F24',
+        'grey-500': '#696868',
+        'grey-300': '#B3B3B3',
+        'grey-100': '#F2F2F2',
+        'green': '#277C78',
+        'yellow': '#F2CDAC',
+        'cyan': '#82C9D7',
+        'navy': '#626070',
+        'red': '#C94736',
+        'purple': '#826CB0',
+        'purple-light': '#AF81BA',
+        'turquoise': '#597C7C',
+        'brown': '#93674F',
+        'magenta': '#934F6F',
+        'blue': '#3F82B2',
+        'navy-grey': '#97A0AC',
+        'army-green': '#7F9161',
+        'gold': '#CAB361',
+        'orange': '#BE6C49',
       },
+      fontFamily: {
+        sans: ['Public Sans', 'sans-serif'],
+        italic: ['Public Sans Italic', 'sans-serif']
+      }
     },
   },
   plugins: [],
