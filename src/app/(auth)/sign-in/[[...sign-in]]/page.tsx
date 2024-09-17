825e87bb4ec666dc30a4f5bd653700ad8d9d787b
DJ Neill <dj.neill856@gmail.com>
2024-09-17 00:47:05 -0500

    continue sign up / in page

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,35 @@
+import { SignIn } from '@clerk/nextjs'
+import TitleBar from '@/app/components/TitleBar'
+import AuthImage from '@/app/components/AuthImage'
+
+export default function Login() {
+    return (
+        <main className='flex flex-col w-full min-h-screen'>
+            <div className='lg:hidden'>
+                <TitleBar />
+            </div>
+
+            <div className='flex flex-col lg:flex-row h-screen'>
+                <div className='hidden lg:flex lg:w-1/2 relative overflow-hidden bg-grey-900 m-2 rounded-xl'>
+                    <div className='absolute inset-0 flex flex-col justify-between p-8 text-white z-10'>
+                        <p className='font-sans text-preset-1'>Money Guardian</p>
+                        <div>
+                            <h2 className='font-sans text-preset-1 mb-4'>Keep track of your money and save for your future</h2>
+                            <p className='font-sans text-preset-4'>Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.</p>
+                        </div>
+                    </div>
+                    <AuthImage
+                        width={560}
+                        height={920}
+                        className='object-cover w-[560px] h-full'
+                    />
+                </div>
+                <div className='flex w-full lg:w-1/2 justify-center items-center h-full'>
+                    <div className='m-auto'>
+                        <SignIn />
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
