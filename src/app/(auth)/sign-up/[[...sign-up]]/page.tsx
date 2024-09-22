94e8b8080fc410007ed4347d64a2db773d9f142e
DJ Neill <dj.neill856@gmail.com>
2024-09-21 21:02:40 -0500

    title component based on pathname

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -33,26 +33,3 @@ export default function Login() {
         </main>
     )
 }
-
-{/* <main className='flex flex-col w-full min-h-screen'>
-<div className='lg:hidden'>
-    <TitleBar />
-</div>
-
-<div className='flex-grow flex flex-col lg:grid lg:grid-cols-2 items-center justify-center px-4'>
-    <div className='hidden lg:block w-full h-screen m-4 overflow-hidden rounded-xl justify-self-center'>
-        <p className='font-sans text-preset-1 absolute p-4 font-[1000]'>Money Guardian finance</p>
-        <p className='font-sans text-preset-1 absolute  bottom-20 text-wrap max-w-[560px]'>Keep track of your money
-            and save for your future</p>
-        <p className='text-preset-4 bottom-10 relative z-10 max-w-[560px]'>Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.</p>
-        <img src="/assets/images/image.png" alt="" className='rounded-xl 2-[560px] h-[920px]' />
-        {/* <AuthImage
-            width={560}
-            height={920}
-        /> */}
-//     </div >
-//     <div className='flex w-full justify-center mx-auto'>
-//         <SignUp />
-//     </div>
-// </div >
-// </main > */}
---
+++ src/app/components/HeaderTitle.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react';
+import { usePathname } from 'next/navigation';
+
+export default function HeaderTitle() {
+    const pathname = usePathname();
+
+    const getTitle = () => {
+        switch (pathname) {
+            case '/':
+                return 'Overview';
+            case '/transactions':
+                return 'Transactions';
+            case '/budgets':
+                return 'Budgets';
+            case '/pots':
+                return 'Pots';
+            case '/recurring-bills':
+                return 'Recurring Bills';
+            default:
+                return 'Money Guardian'
+        }
+    };
+
+    return (
+        <h1 className='text-black text-preset-1 font-sans'>
+            {getTitle()}
+        </h1>
+    )
+}
