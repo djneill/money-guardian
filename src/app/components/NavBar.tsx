40d0f79eec0c16ad3a87d7ab906974e4de9c5a8d
DJ Neill <dj.neill856@gmail.com>
2024-09-23 23:06:46 -0500

    change svg component props. Add usePathname to highlight active tab. Use .map() to map over nav icons and check active pathname.

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,28 +1,43 @@
+'use client'
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import OverviewIcon from '../icons/OverviewIcon';
 import TransactionsIcon from '../icons/TransactionsIcon';
+import BudgetsIcon from '../icons/BudgetsIcon';
+import PotsIcon from '../icons/PotsIcon';
+import BillsIcon from '../icons/BillsIcon';
 
 export default function NavBar() {
+    const pathname = usePathname()
+
+    const navItems = [
+        { href: '/', icon: OverviewIcon },
+        { href: '/transactions', icon: TransactionsIcon },
+        { href: '/budgets', icon: BudgetsIcon },
+        { href: '/pots', icon: PotsIcon },
+        { href: '/bills', icon: BillsIcon },
+
+    ]
     return (
-        <nav className='bg-grey-900 flex rounded-t-xl fixed bottom-0 left-0 right-0 p-4'>
-            <div className="flex justify-around w-full mt-2 mx-4">
-                <ul className='flex flex-row gap-4'>
-                    <li className='w-16 h-11 flex justify-center items-center'>
-                        <Link href={'/'}>
-                            <OverviewIcon />
-                        </Link>
-                    </li>
-                    <li className='w-16 h-11 flex fill-white justify-center items-center'>
-                        <Link href={'/transactions'}>
-                            <TransactionsIcon />
-                        </Link>
-                    </li>
-                    <li className='w-16 h-11 flex justify-center items-center'>
-                        <a href="#" className="text-white">Contact</a>
-                    </li>
-                </ul>
-            </div>
+        <nav className='bg-grey-900 fixed bottom-0 left-0 right-0 px-4 pt-4 rounded-t-xl'>
+            <ul className='flex justify-between items-center'>
+                {navItems.map(({ href, icon: Icon }) => {
+                    const isActive = pathname === href;
+                    return (
+                        <li key={href} className={`flex-1 rounded-t-xl ${isActive ? 'bg-white' : ''}`}>
+                            <Link href={href} className={`block p-4 ${isActive ? 'relative' : ''}`}>
+                                <Icon className='mx-auto'
+                                    fill={isActive ? '#277C78' : '#B3B3B3'} />
+                                {isActive && (
+                                    <div className='absolute bottom-0 left-0 right-0 h-1 bg-green'></div>
+                                )}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </nav>
     )
 }
