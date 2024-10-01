220f11590c99738ae57668d0c5468c7540463f25
DJ Neill <dj.neill856@gmail.com>
2024-09-30 23:39:23 -0500

    continue overview component for balance, income, expense.

--- src/app/components/OverviewCurrentBalance.tsx
+++ src/app/components/OverviewCurrentBalance.tsx
@@ -4,9 +4,23 @@ import { currencyFormat } from '@/utils/currencyFormatter';
 
 export default function OverviewCurrentBalance() {
     return (
-        <div className='flex flex-col bg-grey-900 text-white w-[343px] h-28 rounded-lg justify-center gap-4 px-4 mt-8'>
-            <p className='text-preset-4'>Current Balance</p>
-            <p className='text-preset-1'>{currencyFormat(data.balance.current)}</p>
+        <div>
+            <div className='flex flex-col bg-grey-900 text-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-8'>
+                <p className='text-preset-4'>Current Balance</p>
+                <p className='text-preset-1'>{currencyFormat(data.balance.current)}</p>
+            </div>
+
+            <div className="flex flex-col bg-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-8">
+                <p className="text-preset-4 text-grey-500">Income</p>
+                <p className='text-preset-1 text-grey-900'>{currencyFormat(data.balance.income)}</p>
+            </div>
+
+            <div className="flex flex-col bg-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-8">
+                <p className="text-preset-4 text-grey-500">Expenses</p>
+                <p className='text-preset-1 text-grey-900'>{currencyFormat(data.balance.expenses)}</p>
+            </div>
         </div>
+
+
     )
 }
