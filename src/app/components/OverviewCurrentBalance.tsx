f6d7bc3140285fc1014982f353ced3fe532628bb
DJ Neill <dj.neill856@gmail.com>
2024-10-06 21:55:59 -0500

    Add mobile container to temp pages

--- src/app/components/OverviewCurrentBalance.tsx
+++ src/app/components/OverviewCurrentBalance.tsx
@@ -10,12 +10,12 @@ export default function OverviewCurrentBalance() {
                 <p className='text-preset-1'>{currencyFormat(data.balance.current)}</p>
             </div>
 
-            <div className="flex flex-col bg-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-8">
+            <div className="flex flex-col bg-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-3">
                 <p className="text-preset-4 text-grey-500">Income</p>
                 <p className='text-preset-1 text-grey-900'>{currencyFormat(data.balance.income)}</p>
             </div>
 
-            <div className="flex flex-col bg-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-8">
+            <div className="flex flex-col bg-white w-full h-28 rounded-lg justify-center gap-4 px-4 mt-3">
                 <p className="text-preset-4 text-grey-500">Expenses</p>
                 <p className='text-preset-1 text-grey-900'>{currencyFormat(data.balance.expenses)}</p>
             </div>
--- src/app/pots/page.tsx
+++ src/app/pots/page.tsx
@@ -4,7 +4,9 @@ import NavBar from "../components/NavBar";
 export default function Home() {
     return (
         <main>
-            <HeaderTitle />
+            <div className="mx-4 mt-6">
+                <HeaderTitle />
+            </div>
             <NavBar />
         </main>
     );
