8ae808b42e03049ed90e1b9590e8a19a2d7f877b
DJ Neill <dj.neill856@gmail.com>
2024-10-14 23:05:08 -0500

    begin mobile layout for transaction overview.

--- src/app/components/OverviewTransactions.tsx
+++ src/app/components/OverviewTransactions.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import CaretRight from '../icons/CaretRight';
+import data from '@/app/data/data.json';
+import { currencyFormat } from '@/utils/currencyFormatter';
 
 export default function OverviewTransactions() {
     return (
-        <div>OverviewTransactions</div>
+        <div className='flex flex-col py-6 px-5 w-full rounded-lg bg-white my-4'>
+            <div className="flex flex-row justify-between">
+                <p className='text-black text-preset-2'>Transactions</p>
+                <Link className='flex flex-row items-center' href={'/transactions'} aria-label='see details'>
+                    <p className='text-preset-4 text-gray-500 mr-4'>See Details</p>
+                    <CaretRight />
+                </Link>
+            </div>
+
+
+        </div>
     )
 }
