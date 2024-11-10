4c138d925a878ac56f7467375ae303db47d904ad
DJ Neill <dj.neill856@gmail.com>
2024-11-09 20:40:44 -0600

    update test pages

--- src/app/recurringBills/page.tsx
+++ src/app/recurringBills/page.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import HeaderTitle from "../components/HeaderTitle";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
     return (
-        <main>
+        <main className={`flex-1 transition-all duration-300 ${isSidebarCollapsed ? 'lg:ml-20' : 'lg:ml-64'}`}>
             <div className="mx-4 mt-6">
                 <HeaderTitle />
             </div>
-            <NavBar />
+            <NavBar onSidebarCollapse={setIsSidebarCollapsed} />
         </main>
     );
 }
