7a01e0189d22800831c0f39eb6d8705dd71212a5
DJ Neill <dj.neill856@gmail.com>
2024-11-13 04:54:48 -0600

    reparing auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,40 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { getLoggedInUser } from '@/lib/server/appwrite'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+// import { getLoggedInUser } from '@/lib/server/appwrite'
 
-export async function middleware(request: NextRequest) {
-    const user = await getLoggedInUser()
+// export async function middleware(request: NextRequest) {
+//     const user = await getLoggedInUser()
 
-    if (!user && !request.nextUrl.pathname.startsWith('/signup')) {
-        return NextResponse.redirect(new URL('/signup', request.url))
-    }
-
-    if (user && request.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/dashboard/overview', request.url))
-    }
-
-    return NextResponse.next()
-}
-
-export const config = {
-    matcher: ['/', '/dashboard/:path*']
-}
-// ####### CLERK MIDDLEWARE #######
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-// const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
-
-// export default clerkMiddleware((auth, request) => {
-//     if (!isPublicRoute(request)) {
-//         auth().protect()
+//     if (!user && !request.nextUrl.pathname.startsWith('/signup')) {
+//         return NextResponse.redirect(new URL('/signup', request.url))
 //     }
-// })
+
+//     if (user && request.nextUrl.pathname === '/') {
+//         return NextResponse.redirect(new URL('/dashboard/overview', request.url))
+//     }
+
+//     return NextResponse.next()
+// }
 
 // export const config = {
-//     matcher: [
-//         // Skip Next.js internals and all static files, unless found in search params
-//         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//         // Always run for API routes
-//         '/(api|trpc)(.*)',
-//     ],
+//     matcher: ['/', '/dashboard/:path*']
 // }
+// ####### CLERK MIDDLEWARE #######
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware((auth, request) => {
+    if (!isPublicRoute(request)) {
+        auth().protect()
+    }
+})
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+    ],
+}
