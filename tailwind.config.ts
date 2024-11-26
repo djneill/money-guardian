7ee296c033a134629456ccdf562c4787fea7f034
DJ Neill <dj.neill856@gmail.com>
2024-11-25 21:49:43 -0600

    fix fluid extractor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,14 @@ import type { Config } from "tailwindcss";
 import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  extract,
+  content: {
+    files: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    extract,
+  },
   theme: {
     screens,
     fontSize,
