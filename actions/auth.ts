275f31404bc7f83c15d4fb18fa67a930e6370a4a
DJ Neill <dj.neill856@gmail.com>
2024-11-27 04:11:44 -0600

    google Oauth test, remove trailing /

--- actions/auth.ts
+++ actions/auth.ts
@@ -153,7 +153,7 @@ export async function signUpWithGithub() {
 
 export async function signUpWithGoogle() {
     const { account } = await createAdminClient();
-    const origin = (await headers()).get("origin") || 'https://money-guardian.vercel.app/';
+    const origin = (await headers()).get("origin") || 'https://money-guardian.vercel.app';
 
     const redirectUrl = await account.createOAuth2Token(
         OAuthProvider.Google,
